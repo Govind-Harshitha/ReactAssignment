f2957e773e7d20596aa6b33107e40983b23a610e
Harshitha <govind.harshitha@ltts.com>
2020-01-06 14:33:54 +0530

    'category'

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
 		return (
 			<div>
 				<Router>
-					<MainHeader />
+					<MainHeader  categories={categories} />
 					<Route path="/" exact>
 					<CategoryHeader categories={categories} />
 						<Dashboard />
--- src/components/Header/MainHeader.js
+++ src/components/Header/MainHeader.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import * as FontAwesome from 'react-icons/lib/fa'
-import { Form, FormControl } from 'react-bootstrap';
+import { Form, FormControl, Dropdown } from 'react-bootstrap';
 import './MainHeader.css';
-const MainHeader = () => {
-    return (
-        <nav>
-            
+class MainHeader extends Component {
+    render() {
+        const category = this.props.categories;
+
+        return (
+            <nav>
+
                 <div className="main-header">
                     <div className="logo">
                         <Link to="/">
@@ -18,9 +21,26 @@ const MainHeader = () => {
                         </Link>
                     </div>
                     <div className="header-left">
-                    <div className="inner-content-style">
-                        <span class="cateogories-font">  <FontAwesome.FaTh /></span>
-                        <span >Categories</span>
+                        <div className="inner-content-style">
+
+                            {/* <span >Categories</span> */}
+                            <Dropdown>
+                                <Dropdown.Toggle variant="none" id="dropdown-basic">
+                                    <span class="cateogories-font">  <FontAwesome.FaTh /></span>
+                                    Categories
+                            </Dropdown.Toggle>
+
+                                <Dropdown.Menu>
+                                  
+                                        {
+                                            category.map((category, i) =>                                                   
+                                                    <Dropdown.Item href="#/action-1"><FontAwesome.FaServer />&nbsp;{category.category}
+                                                    </Dropdown.Item>
+                                              
+                                                
+                                            )}
+                                </Dropdown.Menu>
+                            </Dropdown>
                         </div>
                         <div className="navbar-search">
                             <Form inline>
@@ -47,7 +67,7 @@ const MainHeader = () => {
                             </div>
                             </div> */}
                         <div className="navbar-icon">
-                                <FontAwesome.FaShoppingCart className="icon-size" />
+                            <FontAwesome.FaShoppingCart className="icon-size" />
                         </div>
                         {/* <div className="cart">
                             <div className="cart-font">
@@ -63,9 +83,10 @@ const MainHeader = () => {
                     </div>
 
                 </div>
-         
-        </nav>
-    );
+
+            </nav>
+        );
+    }
 };
 
 export default MainHeader;
